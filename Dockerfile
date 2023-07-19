858859c2fd8475a05d7c662f0ce4cc1aa7c06d4b
Aleksandar Stevcic <aleksandarstevcic.as@gmail.com>
2023-07-19 00:01:02 -0400

    edit build

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM nginx:stable-alpine-slim
 ARG CACHEBUST=1
-COPY ./alestev.com/ /usr/share/nginx/html/
+COPY ./ /usr/share/nginx/html/
 RUN ls /usr/share/nginx/html/*
 COPY ./config/ /etc/nginx/conf.d/
--- build/build.sh
+++ build/build.sh
@@ -5,7 +5,8 @@ apikey="ptr_oFozbXfW6ADXChdqujDuzoAC/KgAU2Bsgozhgg9eFuc="
 stackid="23"
 
 #build with the dockerfile in the same folder, and name it alestevcom
-docker build -t alestevcom --no-cache ..
+cd ..
+docker build -t alestevcom --no-cache .
 
 #stop compose file that runs this server
 curl -X POST --insecure -I "https://localhost:9443/api/stacks/$stackid/stop" \
